a7c8c3ac78cd98db9dc0c0f6fb32a3a849ad0f90
zzhelyazkov <zlatinzhelyazkov98@gmail.com>
2023-04-11 20:43:20 +0200

    Rates and productivity blocks done

---
+++ components/secondary/rate.js
@@ -0,0 +1,18 @@
+import styles from "../../src/styles/rate.module.scss";
+
+export default function Rate({ img, title, description, price }) {
+  return (
+    <div className={styles.Rate}>
+      <div className={styles.Rate_inner}>
+        <div className={styles.Rate_image}>
+          <img src={img.src} alt="Rates" />
+        </div>
+        <div className={styles.Rate_content}>
+          <h4>{title}</h4>
+          <p>{description}</p>
+          <span>{price}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Layout from "../../components/main/layout";
 import IntroBlock from "../../blocks/introBlock";
+import RatesBlock from "../../blocks/ratesBlock";
+import ProductivityBlock from "../../blocks/productivityBlock";
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
@@ -10,6 +12,8 @@ export default function Home() {
         <title>Host Rongo - Quick & Secure Web Hosting</title>
       </Head>
       <IntroBlock />
+      <RatesBlock />
+      <ProductivityBlock />
     </Layout>
   );
 }
